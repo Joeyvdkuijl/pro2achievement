7bd9ad5adf11b35dc10a09ea387de6b97b46fab6
Joey van der kuijl <30226@ma-web.nl>
2019-09-27 12:09:06 +0200

    New tekst

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace pro2achievement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Wow!");
+        }
+    }
+}
